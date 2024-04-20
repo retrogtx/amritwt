b16e2b058fd127d986274758a58baa47e2a012a1
Amrit Rai <iamamrit27@gmail.com>
2024-04-20 10:23:00 +0530

    Refactor about page content and metadata, and add new about/page.tsx component

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import { BlogPosts } from 'app/components/posts'
+
+export const metadata = {
+    title: 'about',
+    description: 'who is amrit?',
+}
+
+export default function Page() {
+    return (
+        <section>
+            <h1 className="font-semibold text-2xl mb-8 tracking-tighter">damn, who's this?</h1>
+        </section>
+    )
+}
