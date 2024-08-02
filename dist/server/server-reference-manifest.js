718a2fc4e32f5e798d999f3e97f3744863f9dda2
Amrit Rai <iamamrit27@gmail.com>
2024-08-02 16:15:28 +0530

    remove dist

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"L1X27GgStPLaxDxSXvgqjma/0srLrAb547hYEa5Sb7M=\"}"
--- dist/static/chunks/app/blog/[slug]/page-88f7057dd0184c99.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[308],{153:function(e,n,s){Promise.resolve().then(s.t.bind(s,8399,23)),Promise.resolve().then(s.t.bind(s,4505,23))}},function(e){e.O(0,[505,399,239,740,744],function(){return e(e.s=153)}),_N_E=e.O()}]);
