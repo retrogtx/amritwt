bdbf920529ac5c801dee4197ab39ec6cc89dfdae
Amrit Rai <iamamrit27@gmail.com>
2024-08-02 17:14:11 +0530

    remove nextjs canary

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    // distDir: 'dist', // or any other directory name you prefer
-}
+    swcMinify: true,
+    compiler: {
+      removeConsole: process.env.NODE_ENV !== 'development',
+    },
+  }
